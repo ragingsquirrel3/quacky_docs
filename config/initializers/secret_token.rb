186079bf79d829d2ce8d0473b6559759bcd96790
Travis Sheppard <travissheppard.1988@gmail.com>
2013-01-27 16:28:01 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuackyDocs::Application.config.secret_token = '00ad2f0c60027bae8d175c04bd74eb4a19d3a509058c7e57071053ad49d9a78afdcf2bcf4d2d4a42694d0e139d5b5cdb1457fef85867500d468a615b9ac7c5c2'
